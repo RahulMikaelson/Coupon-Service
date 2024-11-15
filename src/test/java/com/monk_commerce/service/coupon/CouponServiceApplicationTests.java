69be08d0edae220bccae14dcc2127e99c64f21b8
RahulMikaelson <mikaelsonrahul@gmail.com>
2024-11-15 23:15:39 +0530

    Initial Project Setup

---
+++ src/test/java/com/monk_commerce/service/coupon/CouponServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monk_commerce.service.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
